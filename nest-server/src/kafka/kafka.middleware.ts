bffb5e8146e5e8d209efc81898d21855164b25ae
Nero Kwok <nerokwok96@yahoo.com>
2023-06-18 16:11:31 +0800

    hi

--- nest-server/src/kafka/kafka.middleware.ts
+++ nest-server/src/kafka/kafka.middleware.ts
@@ -22,8 +22,11 @@ export class KafkaMiddleware implements NestMiddleware {
     }
   }
   async use(req: Request, res: Response, next: NextFunction) {
-    console.log('request: \n', req.ip);
-    const topic = 'new-pages';
+    next();
+  }
+
+  async logUserLogin(name: string, email: string, req: Request) {
+    const topic = 'user-logins';
     const userAgent = req.headers['user-agent'] || '';
     const os = userAgent.split(/[()]/)[1] || 'Unknown'; // Extract operating system info from user-agent header
     const device = userAgent.match(/(iPhone|iPod|iPad|Android|Windows Phone)/)
@@ -31,35 +34,14 @@ export class KafkaMiddleware implements NestMiddleware {
       : 'Desktop'; // Detect device type based on user-agent header
     const browser = userAgent.split(/[()]/)[2]?.split(' ')[1] || 'Unknown'; // Extract browser info from user-agent header
     const message = {
-      key: 'nest-log-ip',
-      value: JSON.stringify({
-        ip: req.ip,
-        os: os,
-        device: device,
-        browser: browser,
-      }),
-    };
-    const messages = Array(1).fill(message);
-    try {
-      await this.producer
-        .send({
-          topic,
-          messages: messages,
-        })
-        .then(console.log);
-    } catch (e) {
-      console.log('[error]', e);
-    }
-    next();
-  }
-
-  async logUserLogin(name: string, email: string) {
-    const topic = 'user-logins';
-    const message = {
       key: 'user-login',
       value: JSON.stringify({
         name: name,
         email: email,
+        ip: req.ip,
+        os: os,
+        device: device,
+        browser: browser,
         time: new Date().toISOString(),
       }),
     };
